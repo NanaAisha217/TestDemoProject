0bc94f2b7c067b82889cdaf480f04956817efb96
Aishat Ibrahim <aishat.ibrahim@optimusbank.com>
2025-04-25 17:08:29 +0100

    Initial commit - test deployment

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() => "Hello, I am trying out this deployment";
+    }
+}
